62b0d4ef3566dc2c1d36704b3881d57f98851047
horahora <contact@horahora.com>
2022-10-31 04:20:05 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  unstable_staticImage: true,
+});
+module.exports = withNextra();
